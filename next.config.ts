040bee362c72f86b3ddcbdc9236627ee132055ec
jastincito <jastinalexisramirezsaguio@gmail.com>
2026-02-20 00:14:00 -0500

    Arreglando error de rutas dinamicas

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // <--- AGREGA ESTA LÍNEA
   images: {
     unoptimized: true, // <--- ESTA TAMBIÉN para que carguen las fotos
   },
